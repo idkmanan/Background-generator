39cdbbf3a4162aa6d41d4e88078781b8cd1ea8e0
its-Manan <mananagg2004@gmail.com>
2023-08-24 17:02:11 +0530

    Test run

--- script.js
+++ script.js
@@ -1,3 +1,6 @@
+var _ = require('lodash');
+var arr = [1,2,3,4,5,6,7,8]
+console.log("Array:", _.without(arr, 3));
 var css = document.querySelector("h3");
 var color1 = document.querySelector(".color1");
 var color2 = document.querySelector(".color2");
